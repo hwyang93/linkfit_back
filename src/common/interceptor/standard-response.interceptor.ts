f944be819d1eb615ee293dd135afd8abf6ff5277
yanghw <yanghw0911@gmail.com>
2022-11-15 23:38:35 +0900

    - standardResponse interceptor 추가 - swagger 기본설정추가

---
+++ src/common/interceptor/standard-response.interceptor.ts
@@ -0,0 +1,15 @@
+import { CallHandler, ExecutionContext, Injectable, NestInterceptor } from '@nestjs/common';
+import {map, Observable} from 'rxjs';
+
+@Injectable()
+export class StandardResponseInterceptor implements NestInterceptor {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    return next.handle().pipe(
+        map((data) => ({
+          httpStatusCode: context.switchToHttp().getResponse().statusCode,
+          message: 'success',
+          data: data,
+        })),
+    );
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {DocumentBuilder, SwaggerModule} from "@nestjs/swagger";
+
+declare const module: any;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const port = process.env.PORT || 3000;
+
+  const config = new DocumentBuilder().setTitle('linkfit API').setDescription('linkfit API 문서입니다.').setVersion('1.0').build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document)
+
+  await app.listen(port);
+
+  console.log(`listening on port ${port}`);
+
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+    console.log('hot reload.....');
+  }
 }
 bootstrap();
